7ac7df56336de540fa435299eca392e9df0e34d5
Jasper <j.kuiper@summaview.nl>
2018-10-09 12:05:30 +0200

    Pils

---
+++ WebContent/WEB-INF/jsp/pils.jsp
@@ -0,0 +1,11 @@
+<%@ include file="header.jspf"%>
+
+<body>
+
+<%@ include file="navbar.jspf"%>
+
+<p> haha hier komt pils xd </p>
+
+</body>
+
+<%@ include file="footer.jspf"%>
---
+++ src/Servlet/LoginServlet.java
@@ -0,0 +1,51 @@
+package Servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Beans.LoginBean;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		/*StudentBean studentBean = new StudentBean();
+		studentBean.setFirstName("Jasper");
+		studentBean.setLastName("Kuiper");
+		studentBean.setAge(22);
+		
+		request.setAttribute("student", studentBean);*/
+		
+		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
